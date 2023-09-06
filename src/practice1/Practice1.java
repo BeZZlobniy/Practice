762c2c649d89cdadfe6cd1663ab6b291c8720474
Bezzlobniy <sharp-georg@yandex.ru>
2023-09-06 21:57:44 +0300

    File 1

---
+++ src/practice1/Practice1.java
@@ -0,0 +1,18 @@
+package practice1;
+import java.util.Scanner;
+
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите аргументы через пробел: ");
+        String inLine = in.nextLine();
+        if (inLine.length == 0) {
+            System.out.println("Вы не ввели аргументы");
+        } else {
+            for (int i = 0; i < inLine.length; i++) {
+                System.out.println(args[i]);
+            }
+        }
+    }
+}
