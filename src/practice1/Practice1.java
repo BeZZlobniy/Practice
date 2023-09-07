d1f4d2e05b3bb8ce6e3b4aad2efad3d07f71383b
Bezzlobniy <sharp-georg@yandex.ru>
2023-09-07 22:31:18 +0300

    make something

--- src/practice1/Practice1.java
+++ src/practice1/Practice1.java
@@ -4,14 +4,10 @@ import java.util.Scanner;
 
 public class Practice1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите аргументы через пробел: ");
-        String inLine = in.nextLine();
-        String[] array = inLine.split(" ");
-
-
-        for (String s : array) {
-            System.out.println(s);
+        // Перебираем все аргументы входной строки
+        for (int i = 0; i < args.length; i++) {
+            // Выводим аргумент на экран
+            System.out.println("Аргумент " + (i+1) + ": " + args[i]);
         }
     }
 }
