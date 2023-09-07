3eba9d5214ecf9faff0c6cd9b3e55f94988a12ee
BeZZlobniy <sharp-georg@yandex.ru>
2023-09-07 19:36:55 +0300

    Make shit

--- src/practice1/practice1.java
+++ src/practice1/practice1.java
@@ -1,8 +1,18 @@
 package practice1;
-import java.util.SortedMap;
+import java.util.Scanner;
 
-public class practice1 {
-    public static void main(String[] args){
-        System.out.println("Tell me a story");
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите аргументы через пробел: ");
+        String inLine = in.nextLine();
+        if (inLine.length() == 0) {
+            System.out.println("Вы не ввели аргументы");
+        } else {
+            for (int i = 0; i < inLine.length(); i++) {
+                System.out.println(args[i]);
+            }
+        }
     }
 }
