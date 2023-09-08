5e964325ee4e183d35e568450a4ec51798b10078
BeZZlobniy <sharp-georg@yandex.ru>
2023-09-08 09:45:40 +0300

    Make shit

---
+++ src/practice2/TestAuthor.java
@@ -0,0 +1,8 @@
+package practice2;
+
+public class TestAuthor{
+    public static void main(String[] args) {
+        Author author = new Author("John Smith", "john.smith@example.com", 'M');
+        System.out.println(author);
+    }
+}
