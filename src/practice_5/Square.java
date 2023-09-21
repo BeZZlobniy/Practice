8668f1306136e2a04b39ce490705b4728f047e07
BeZZlobniy <sharp-georg@yandex.ru>
2023-09-21 22:41:18 +0300

    Make something

---
+++ src/practice_5/Square.java
@@ -0,0 +1,44 @@
+package practice_5;
+
+public class Square extends Rectangle {
+    public Square() {
+        super();
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public void setSide(double side) {
+        width = side;
+        length = side;
+    }
+
+    public double getSide() {
+        return width;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
